6c1a9fe8538d3331d50a7588f90c1c787b48fb25
Znium <crancee.eliott@gmail.com>
2023-12-13 17:29:43 +0100

    modified:   wave2vec.py

--- wave2vec.py
+++ wave2vec.py
@@ -1,3 +1,20 @@
 import torchaudio
+import wave
+from load_data import *
+from transformers import Wav2Vec2Model, Wav2Vec2Processor
 
-s = torchaudio.load("Dataset/audio/1_channels/AAOR_OR.mp4")
+mp4_file_path = "Dataset/audio/2_channels/AA_OR.wav"
+
+data = load_all_ipus("Dataset/transcr")
+
+model_name = "facebook/wav2vec2-base-960h"
+processor = Wav2Vec2Processor.from_pretrained(model_name)
+model = Wav2Vec2Model.from_pretrained(model_name)
+
+def get_embeddings(n, mp4_file_path) : 
+    audio_tensor, frame_rate = torchaudio.load(mp4_file_path)
+    start = int(frame_rate*data["start"][n])
+    stop = int(frame_rate*data["stop"][n])
+    sample_tensor = audio_tensor[:,start:stop]
+    result = model.forward(sample_tensor).last_hidden_state
+    return(result)
